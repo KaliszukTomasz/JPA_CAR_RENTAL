f771034cfbabfd02e2e3838193425ac77c1bc069
KaliszukTomasz <tomasz-kaliszuk@wp.pl>
2018-08-16 16:54:23 +0200

    EmployeeDao with implementation to add and remove employee to and from office.

--- src/main/java/com/capgemini/dao/EmployeeDao.java
+++ src/main/java/com/capgemini/dao/EmployeeDao.java
@@ -6,8 +6,14 @@ import com.capgemini.domain.EmployeeEntity;
 
 public interface EmployeeDao extends Dao<EmployeeEntity, Long> {
 //TODO
+//	
+//	ArrayList<Long> findListOfEmployeesConnectedWithCar(Long carId);
+//	
+//	
 	
-	ArrayList<Long> findListOfEmployeesConnectedWithCar(Long carId);
-	
+
+	void addEmployeeToOffice(Long officeId, Long employeeId);
+
+	void removeEmployeeFromOffice(Long officeId, Long employeeId);
 	
 }
--- src/main/java/com/capgemini/dao/impl/EmployeeDaoImpl.java
+++ src/main/java/com/capgemini/dao/impl/EmployeeDaoImpl.java
@@ -1,14 +1,36 @@
 package com.capgemini.dao.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.capgemini.dao.EmployeeDao;
+import com.capgemini.dao.OfficeDao;
 import com.capgemini.domain.EmployeeEntity;
+import com.capgemini.domain.OfficeEntity;
 
 @Repository
 public class EmployeeDaoImpl extends AbstractDao<EmployeeEntity, Long> implements EmployeeDao {
 
-	//TODO
+	
+	@Autowired
+	OfficeDao officeDao;
+	
+	
+	@Override
+	public void addEmployeeToOffice(Long officeId, Long employeeId) {
+		EmployeeEntity employeeEntity = findOne(employeeId);
+		OfficeEntity officeEntity = officeDao.findOne(officeId);
+		officeEntity.addEmployeeEntity(employeeEntity);
+		
+	}
+	
+	@Override
+	public void removeEmployeeFromOffice(Long officeId, Long employeeId){
+		
+		
+	}
+
+	
 	
 
 }
