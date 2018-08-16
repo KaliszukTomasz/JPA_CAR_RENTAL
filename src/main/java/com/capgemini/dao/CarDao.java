b3faec77c1538d55fc22bf0849543e2788fb32be
KaliszukTomasz <tomasz-kaliszuk@wp.pl>
2018-08-16 16:55:25 +0200

    New search ListOfEmployeesInOfficeAssignedToCar.

--- src/main/java/com/capgemini/dao/CarDao.java
+++ src/main/java/com/capgemini/dao/CarDao.java
@@ -11,7 +11,10 @@ public interface CarDao extends Dao<CarEntity, Long> {
 	List<CarEntity> findCarByBrandAndType(String brand, String type);
 	Set<CarEntity> findCarByOffice(Long officeId);
 	CarEntity addAttachedEmployee(Long carId, EmployeeEntity employeeEntity);
-	Set<CarEntity> findCarByEmployee(Long employeeId);
+	
+	List<EmployeeEntity> findListOfEmployeesInOfficeAssignedToCar(Long officeId, Long carId);
+	
+	
 	
 
 }
--- src/main/java/com/capgemini/dao/impl/CarDaoImpl.java
+++ src/main/java/com/capgemini/dao/impl/CarDaoImpl.java
@@ -1,17 +1,17 @@
 package com.capgemini.dao.impl;
 
-import java.security.spec.EncodedKeySpec;
 import java.util.List;
 import java.util.Set;
 
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.capgemini.dao.CarDao;
 import com.capgemini.dao.EmployeeDao;
 import com.capgemini.dao.OfficeDao;
-import com.capgemini.domain.BookEntity;
 import com.capgemini.domain.CarEntity;
 import com.capgemini.domain.EmployeeEntity;
 import com.capgemini.domain.OfficeEntity;
@@ -19,39 +19,51 @@ import com.capgemini.domain.OfficeEntity;
 @Repository
 public class CarDaoImpl extends AbstractDao<CarEntity, Long> implements CarDao {
 
+	@Autowired
+	OfficeDao officeDao;
+	@Autowired
+	EmployeeDao employeeDao;
+
 	@Override
 	public List<CarEntity> findCarByBrandAndType(String brand, String type) {
 		TypedQuery<CarEntity> query = entityManager.createQuery(
 				"select car from CarEntity car where upper(car.brand) "
-				+ "like concat(upper(:brand), '%') AND upper(car.type) "
-				+ "like concat(upper(:type),'%')", CarEntity.class);
+						+ "like concat(upper(:brand), '%') AND upper(car.carType) " + "like concat(upper(:type),'%')",
+				CarEntity.class);
 		query.setParameter("brand", brand);
 		query.setParameter("type", type);
 		return query.getResultList();
-		
+
 	}
 
 	@Override
 	public Set<CarEntity> findCarByOffice(Long officeId) {
-		OfficeDao officeDao = new OfficeDaoImpl();
 		OfficeEntity officeEntity = officeDao.findOne(officeId);
 		return officeEntity.getCarEntitySet();
 	}
 
 	@Override
 	public CarEntity addAttachedEmployee(Long carId, EmployeeEntity employeeEntity) {
-		CarDao carDao = new CarDaoImpl();
-		CarEntity carEntity = carDao.findOne(carId);
+		CarEntity carEntity = findOne(carId);
 		carEntity.addEmployeeEntityToCarEntity(employeeEntity);
-		employeeEntity.addCarEntity(carEntity);
+		EmployeeEntity newEmployeeEntity = employeeDao.findOne(employeeEntity.getId());
+		newEmployeeEntity.addCarEntity(carEntity);
+		update(carEntity);
+		employeeDao.update(newEmployeeEntity);
+
 		return carEntity;
 	}
 
 	@Override
-	public Set<CarEntity> findCarByEmployee(Long employeeId) {
-		EmployeeDao employeeDao = new EmployeeDaoImpl();
-		EmployeeEntity employeeEntity = employeeDao.findOne(employeeId);
-		return employeeEntity.getCarsSet();
+	public List<EmployeeEntity> findListOfEmployeesInOfficeAssignedToCar(Long officeId, Long carId) {
+		
+		TypedQuery<EmployeeEntity> query = entityManager
+				.createQuery("select employee from EmployeeEntity employee left join employee.carsSet as cars "
+						+ "where (employee.office.id) = :officeId AND (cars.id) = :carId", EmployeeEntity.class);
+		query.setParameter("officeId", officeId);
+		query.setParameter("carId", carId);
+		return query.getResultList();
+
 	}
 
 }
