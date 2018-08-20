a26c12b5a07edef4bfcc17d8f187b7fadae36d48
KaliszukTomasz <tomasz-kaliszuk@wp.pl>
2018-08-20 11:45:46 +0200

    New method addNewCarLoanToDatabase in carLoanDao.

--- src/main/java/com/capgemini/dao/CarLoanDao.java
+++ src/main/java/com/capgemini/dao/CarLoanDao.java
@@ -1,8 +1,12 @@
 package com.capgemini.dao;
 
+import com.capgemini.domain.CarEntity;
 import com.capgemini.domain.CarLoanEntity;
+import com.capgemini.domain.ClientEntity;
 
 public interface CarLoanDao extends Dao<CarLoanEntity, Long> {
+
+	void addNewCarLoanToDatabase(CarLoanEntity carLoanEntity, ClientEntity clientEntity, CarEntity carEntity);
 
 	//TODO
 	
--- src/main/java/com/capgemini/dao/impl/CarLoanDaoImpl.java
+++ src/main/java/com/capgemini/dao/impl/CarLoanDaoImpl.java
@@ -1,14 +1,25 @@
 package com.capgemini.dao.impl;
 
-import javax.transaction.Transactional;
-
 import org.springframework.stereotype.Repository;
 
 import com.capgemini.dao.CarLoanDao;
+import com.capgemini.domain.CarEntity;
 import com.capgemini.domain.CarLoanEntity;
+import com.capgemini.domain.ClientEntity;
 
 @Repository
 public class CarLoanDaoImpl extends AbstractDao<CarLoanEntity, Long> implements CarLoanDao {
 
-	//TODO
+	
+	
+	@Override
+	public void addNewCarLoanToDatabase(CarLoanEntity carLoanEntity, ClientEntity clientEntity, CarEntity carEntity){
+		
+		CarLoanEntity carLoanEntity2 = save(carLoanEntity);
+		carLoanEntity2.setCar(carEntity);
+		carLoanEntity2.setClient(clientEntity);
+	
+		
+		
+	}
 }
